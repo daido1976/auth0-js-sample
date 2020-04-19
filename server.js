e4d9b3f083b9f3325b629605eec24c5d31937191
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-04-19 14:32:09 +0900

    Add a protected endpoint

--- server.js
+++ server.js
@@ -21,14 +21,28 @@ const checkJwt = jwt({
 // Serve static assets from the /public folder
 app.use("/public", express.static(join(__dirname, "public")));
 
+// Serve the index page for all other requests
+app.get("/", (_, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
 // Endpoint to serve the configuration file
 app.get("/auth_config.json", (req, res) => {
   res.sendFile(join(__dirname, "auth_config.json"));
 });
 
-// Serve the index page for all other requests
-app.get("/*", (_, res) => {
-  res.sendFile(join(__dirname, "index.html"));
+// API only authenticated users can call
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({ msg: "Yeah! Your access token was successfully validated!" });
+});
+
+// Handle error
+// Note: エラーが発生しうる Routing より後に書かないといけない？
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token‍" });
+  }
+  next(err, req, res);
 });
 
 // Listen on port 3000
